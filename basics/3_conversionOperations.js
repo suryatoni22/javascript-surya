7069fb4d54ab9be189dd0d9345f71bc88fe78e1f
suryatoni22 <162897851+suryatoni22@users.noreply.github.com>
2024-10-15 06:41:55 +0000

    update

---
+++ basics/3_conversionOperations.js
@@ -0,0 +1,39 @@
+let age= "123"
+
+let ageconversion= Number(age) //age should be number if not it show NaN
+console.log(typeof age)
+console.log(typeof ageconversion )
+console.log(ageconversion)
+console.log(age)
+
+//"33"- 33
+// "32abc" => NaN
+// null => 0
+// true => 1 or false => 0
+
+let sname = null
+let snameconversion=Number(sname)
+console.log(typeof sname);
+console.log(typeof snameconversion);
+console.log(sname)
+console.log(snameconversion)
+
+let isloggeedin= 1 
+let booleanisloggedin= Boolean(isloggedin)
+console.log(booleancity);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "surya" => true
+
+let school = 33
+let stringschool= String(school)
+console.log(stringschool);
+console.log(typeof stringschool);
+
+// 33=> 33 datatype string
+
+
+
+
