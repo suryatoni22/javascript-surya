ea028d6db0259330d97f18f4cf14c80c3a5e1c1c
suryatoni22 <162897851+suryatoni22@users.noreply.github.com>
2024-10-15 06:04:57 +0000

    updated

---
+++ basics/2_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all js codes new version
+
+// alert (3+3)  // we are using node js, not browser
+
+console.log(4+
+    5
+)   // code should be readable always use readable way
+
+let name = "surya"
+let age = 18
+let isloggedIn = false
+let state;
+
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "")
+console.log(typeof "surya");
+console.log(typeof null);// datatype is object mvp
+console.log(typeof undefined);// undefined datatype is datatype mvp
+console.log(typeof age);
+
+
+
+
+
