405e64447d2b8eb280b43ab0b24dfe1826581c6c
suryatoni22 <162897851+suryatoni22@users.noreply.github.com>
2024-10-15 07:23:38 +0000

    updated

---
+++ basics/4_operations.js
@@ -0,0 +1,16 @@
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**5)
+// console.log(2/3)
+// console.log(2%2)
+
+// console.log(2+"2")
+// console.log("2"+34)
+// console.log(2+2+"32")
+// console.log("45"+2+2)
+
+let gamecounter = 100
+++gamecounter; // we can also use gamecounter++;
+console.log(gamecounter);
+
