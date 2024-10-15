7494a7d587e09592d3f6cbd26e215eb52bd18910
suryatoni22 <162897851+suryatoni22@users.noreply.github.com>
2024-10-15 08:49:55 +0000

    completed

---
+++ basics/5_comparison.js
@@ -0,0 +1,19 @@
+// console.log(5>2)
+// console.log(5<2)
+// console.log(5==2)
+// console.log(5>=2)
+// console.log(5<=2)
+// console.log(5!=2)
+
+console.log(null==0)
+console.log(null>0)
+console.log(null<0)
+console.log(null>=0)
+console.log(null<=0)
+
+console.log(undefined==0)
+console.log(undefined<=0)
+console.log(undefined>=0)
+
+console.log("1"==1)
+console.log("1"===1) // strict comparison where it also compare datatype
