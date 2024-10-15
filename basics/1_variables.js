5a1eaff5cd850b3e8ffcd36ba4cdc73640a3f7de
suryatoni22 <162897851+suryatoni22@users.noreply.github.com>
2024-10-15 05:16:49 +0000

    changes updated 2

---
+++ basics/1_variables.js
@@ -0,0 +1,19 @@
+const accountId = 97971
+let accountEmail = "surya@google.com"
+var accountPassword = "12345"
+accountCity = "Jammu"// this is not good way to define variable always use let
+
+// accountId=2 // not allowed
+
+accountEmail = "ashish.com"
+accountPassword = "33443"
+accountCity= "samba"
+
+console.log(accountId)
+console.table([accountEmail, accountId, accountPassword, accountCity]);
+
+/*
+never use var prefer let
+as it create problem in block scope and functions
+
+*/
