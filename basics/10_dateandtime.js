f2cc412c3ba4149ccba186cd5a599b8fd8d48980
suryatoni22 <162897851+suryatoni22@users.noreply.github.com>
2024-10-16 06:32:08 +0000

    updated

---
+++ basics/10_dateandtime.js
@@ -0,0 +1,10 @@
+const mydate= new Date()
+console.log(mydate.getFullYear())
+console.log(mydate.getDate())
+console.log(mydate.getTime())
+
+let mynewdate= new Date(2004,8,22)
+console.log(mynewdate.toString());
+console.log(mynewdate.toLocaleString())
+
+
