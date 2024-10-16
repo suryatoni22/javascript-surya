3b08608f10dc847de705371121d0e006ebf273c7
suryatoni22 <162897851+suryatoni22@users.noreply.github.com>
2024-10-16 05:55:28 +0000

    save

---
+++ basics/9_numbers.js
@@ -0,0 +1,25 @@
+const balance = new Number (1000.34445)
+console.log(balance.toString().length);
+console.log(balance.toFixed(5));
+console.log(balance.toPrecision(5))
+console.log(balance.toLocaleString(`en-IN`))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.isInteger)
+console.log(Number.length)
+
+//++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++
+
+console.log(Math.abs(-59873));
+console.log(Math.sin(90));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*100 + 1));
+
+const max=100
+const min=50
+console.log(Math.floor(Math.random()*(max-min) + min));
+
+
+
+
+
