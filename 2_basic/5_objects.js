b91fd0ed70eba26c88fa1857c5cb4b08f12bff57
suryatoni22 <162897851+suryatoni22@users.noreply.github.com>
2024-10-17 04:07:50 +0000

    save

---
+++ 2_basic/5_objects.js
@@ -0,0 +1,14 @@
+const course = {
+    coursename:"shaurya batch",
+    price: "999",
+    courseinstructor:"Surya"
+}
+
+const{courseinstructor}= course
+console.log(courseinstructor);  // destructure of objects
+
+// {
+//     "name":"Surya",
+//     "coursename":"CDS"    json structure it is in object
+// }
+
